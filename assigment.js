6993a270b28804e328e439f75d698b81dbf90e5b
Salman <m.salmank9991@gmail.com>
2020-06-14 02:35:28 -0700

    JsAssigment

---
+++ assigment.js
@@ -0,0 +1,186 @@
+// alert(" JavaScript Alert \n Error! please enter a valid password")
+// alert("JavaScript Alert \n \n Welcom to Js Land \n Happy coding")
+
+// alert("JavaScript Alert \n\n Welcom to Js Land")
+// alert("Javascript Alert \n\n Happy coding \nprevent this page from creating additional dialogs")
+
+// Q7 ANS))Body (inside your page's HTML)
+
+// Chapter #2
+
+
+// var username;
+// var myname="Muhammad Salman Khan";
+// var message="Hello World";
+// alert(message);
+// alert("Jhon Doe");
+// alert("15 Years Old");
+// alert("Certified Mobile Application Development");
+
+// var pattren="\r pizza \n pizz \n piz \n pi \n p";
+// alert(pattren);
+
+// var email="example@example.com";
+// alert("My email address is "+email);
+
+// var book="A smarter way to learn JavaScript";
+// alert("I am trying to learn from the book "+book);
+
+// var str="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(str);
+
+
+// Chapter 3
+
+// age=21;
+// alert(age);
+
+// var birthYear=1999;
+// document.write("My birth year is"+birthYear+" \n Data type of my declared variable is number ");
+
+// visitorsName="Jhon Doe";
+// productTitle="T-Shirts";
+// Quantity="5";
+// document.write(visitorsName + " ordered "+Quantity + productTitle + "(s) on XYZ Clothing store")
+
+// Chapter 4
+
+// var name,sirname,fathername;
+
+// legal variale
+
+// 1) var name;
+// 2) var sir_name;
+// 3) var $fatherName;
+// 4) var your_5Semester;
+// 5)var my_name_is;
+
+// Illegal variable
+
+// 1) var 2ndNUmber;
+// 2)var #543;
+// 3) var .name;
+// 4)var 9.year;
+// 5) var *sirname
+
+// document.write("A heading stating Rules for naming JS variables");
+// document.write(" Variable names can only contain numbers,$, and _. for example any_1stvariable" );
+// document.write(" Variables must begin with a letter, $ or _. For example $name, _name or name ");
+// document.write("Variable names are case Sensitive");
+// document.write("Variable names should not be JS Keywords")
+
+// Chapter 5
+
+// var a=3;
+// var b=5;
+// var c=a+b;
+// document.write("Sum of "+a+" and " +b+" is "+c);
+
+// var a=3;
+// var b=5;
+// var c=a-b;
+// document.write("Subtraction of "+a+" and " +b+" is "+c);
+
+// var a=3;
+// var b=5;
+// var c=a*b;
+// document.write("multiplication of "+a+" and " +b+" is "+c);
+
+// var a=3;
+// var b=5;
+// var c=a%b;
+// document.write("modulus of "+a+" and " +b+" is "+c);
+
+
+// var a;
+// document.write("Value after variable declaration is "+a+"<br> ");
+// a=5;
+// document.write(" \n Initial value" +a);
+// ++a;
+// document.write("Value after increment is: " +a +"<br> ")
+// a=a+7;
+// document.write("Value after addition is "+a +"<br> ");
+// --a;
+// document.write("Value after decrement is: "+a +"<br> ");
+
+// a=a%3;
+
+// document.write("The remainder is : "+a +"<br> ");
+
+// ticket_price=600;
+// quantity=5;
+// cost_of_buying_Ticket=ticket_price*quantity;
+// document.write("Total cost to buy"+quantity+"to a movie is "+cost_of_buying_Ticket+"PKR");
+// var num=4;
+// document.write("Table of "+num+"<br>");
+// for(var i=1;i<=10;i++){
+//      document.write( num +" X "+ i + " = "+4*i+"<br>")
+// }
+
+// var celcius=prompt("Enter celcius temperature");
+// fahrenheight=(celcius*(9/5))+32;
+// document.write(celcius+" *C " +" is "+fahrenheight);
+
+// var fahrenheight=prompt("Enter fahrenheight temperature");
+// celcius=(fahrenheight-32)*(5/9);
+// document.write(fahrenheight+" *F " +" is "+celcius+ "*C");
+
+// 7.  Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store the following in variables 
+
+// var price_of_item1=prompt("Enter first item");
+// var quantity_of_1item=prompt("Enter quantity of first item");
+// var price_of_item2=prompt("Enter second item");
+// var quantity_of_2item=prompt("Enter quantity of second item");
+// var shipping_charges=100;
+// document.write("price_of_item1_is="+price_of_item1+"<br>");
+// document.write("quantity_of_item1_is="+quantity_of_1item+"<br>");
+// document.write("price_of_item2_is="+price_of_item2+"<br>");
+// document.write("quantity_of_item2_is="+quantity_of_2item+"<br>");
+// document.write("Shipping Charges is"+shipping_charges);
+// total=(price_of_item1*quantity_of_1item)+(price_of_item2*quantity_of_2item)+shipping_charges;
+// document.write("Total cost of your order is= "+total)
+
+// var total_marks=850;
+// var obtained_marks=400;
+// var percentage;
+// percentage=(obtained_marks/total_marks)*100;
+// document.write("Total Marks "+total_marks+"<br>");
+// document.write("Marks Obtained "+obtained_marks+"<br>");
+// document.write("Percentage "+percentage);
+
+// var ExchangeRate=(10*104.80)+(25*28);
+// document.write("Total Currency in PKR:"+ExchangeRate);
+
+// var num=4;
+// var result=((num+5)*10)/2;
+// console.log(result);
+
+// var currentYear=prompt("Enter Birth Year");
+// var birthYear=prompt("Enter birth Year");
+// age=currentYear-birthYear;
+// document.write("Current Year "+currentYear+"<br>");
+// document.write("Birth Year "+birthYear+"<br>");
+// document.write("Your Age is "+age)
+
+// Geometrizer
+
+// var radius=prompt("Enter radius");
+
+// var circumFerence=2*3.142*radius;
+// document.write("The Circumference is "+circumFerence+"<br>");
+
+// var Area=3.142*radius*radius;
+// document.write("The Area is "+Area);
+
+// var favSnake="Chocolate chip";
+// var CurrAge=21;
+// var MaxAge=65;
+// var EstimatedAmount=3;
+// var total=(MaxAge-CurrAge)*EstimatedAmount;
+// document.write("Favourite Snack "+favSnake+"<br>");
+// document.write("Current Age "+CurrAge+"<br>");
+// document.write("Maximum Age "+MaxAge+"<br>");
+// document.write("Amount of Snacks Per Day "+EstimatedAmount+"<br>");
+// document.write("You will need "+total+" Chocolate chip to last you until the rip old age of "+MaxAge);
+
+
